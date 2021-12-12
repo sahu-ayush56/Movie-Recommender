7e330a8e9900aa0ee1ad91778c7c66b156524f87
sahu-ayush56 <ayushsahu84@gmail.com>
2021-12-12 16:31:55 +0530

    Deployed

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import difflib
-from flask import Flask
-from flask import render_template, url_for, request, redirect
 import requests
 from bs4 import BeautifulSoup
-
-app = Flask(__name__)
-def recommender(movie_name):
+import streamlit as st
+title_match = ""
+def recommender(movie_name,number):
+    global title_match
     movies_df = pd.read_csv('movies.csv')
     features = ['genres','keywords', 'tagline','cast','director']
 
@@ -27,7 +26,7 @@ def recommender(movie_name):
     index_movie = movies_df[movies_df['title'] == title_match]['index'].values[0]
     similarity_movies = list(enumerate(similarity[index_movie]))
     sorted_similarity = sorted(similarity_movies,key = lambda x:x[1],reverse = True)
-    related_movies = list(i[0] for i in sorted_similarity)[1:10]
+    related_movies = list(i[0] for i in sorted_similarity)[1:number+1]
     recommended_movies = list(movies_df[movies_df.index == index]['title'].values[0] for index in related_movies)
     # for index in related_movies:
     #     recommended_movies = movies_df['title']
@@ -44,21 +43,21 @@ def links(movie_name):
         if '/movie/' in tag['href']:
             return tag['href']
 
+def Output(movie_name,number):
+    try:
+        movies = recommender(movie_name,number)
+        st.write("Closest match in the dataset: "+title_match)
+        for movie in movies:
+            movie_link = links(movie.replace(' ','-'))
+            if movie_link!=None:
+                st.write(movie+"- "+("https://www6.f2movies.to"+movie_link))
+    except:
+        st.write("Please enter different movie name")
 
-@app.route("/",methods=["GET","POST"])
-def GUI():
-    if request.method=="POST":
-        movie_name = request.form["movie"]
-        try:
-            movies = recommender(movie_name)
-            for movie in movies:
-                movie_link = links(movie.replace(' ','-'))
-                if movie_link!=None:
-                    print(movie+"- "+"https://www6.f2movies.to"+movie_link)
-        except:
-            print("Please enter different movie name")
-        return redirect(request.url)
-    return render_template('index.html')
+st.title("Movie Recommender")
+movie_name = st.text_input("Enter your favourite movie")
+number = st.slider("No. of recommendations: ", max_value=30)
+if movie_name!="":
+    Output(movie_name,number)
 
-if __name__=="__main__":
-    app.run(debug=True)
+
